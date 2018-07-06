cf2518c62e2876a9517912b128e82d8732298297
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-07 00:03:48 +0300

    connection_depth() docs

--- muskrat/filters.py
+++ muskrat/filters.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from .defaults import max_connection_depth
+
 
 def by_type(object_type):
     return lambda obj: obj.object_type == object_type
@@ -13,9 +15,25 @@ def has_childs():
     return scan_childs
 
 
-def connection_depth(number):
+def connection_depth(value=None, compare=None):
+    """
+    Generates function which compares connection depth of the element with the given value.
+    :param value: value to compare to produce equality expression
+    :param compare: expression (function) which takes depth as argument and returns boolean
+    :return: function (function wrapper)
+    """
+    if value is None and compare is None:
+        raise ValueError()
+
     def get_depth(obj):
-        inf = 9999
-        last, counter = obj.dive(counter=inf)
-        return inf - counter
+        last, counter = obj.dive(counter=max_connection_depth)
+        depth = max_connection_depth - counter
+        if compare is None:
+            return depth == value
+        else:
+            return compare(depth)
     return get_depth
+
+
+
+
