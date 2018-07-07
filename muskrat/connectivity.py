c18fd2f3ae92de7a15f717bd885abd5ef78be2e7
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-07 17:20:30 +0300

    +connectivity.Accept

---
+++ muskrat/connectivity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from .filters import unify
+from collections import namedtuple
+
+
+class Accept:
+    def __init__(self):
+        self.__options = {}
+        self.__default = None
+
+    def add_option(self, fltr, connect=True, insert=True):
+        self.__options[fltr] = dict(connect=connect, insert=insert)
+
+    def add_default(self, connect=True, insert=True):
+        self.__default = dict(connect=connect, insert=insert)
+
+    def get_policy(self, obj):
+        allowed_opts = namedtuple('AllowedOptions', 'connect insert')
+        for (flt, options) in self.__options.items():
+            if unify(flt)(obj):
+                return allowed_opts(options['connect'], options['insert'])
+        if self.__default is None:
+            raise MissingDefault()
+        return allowed_opts(self.__default['connect'], self.__default['insert'])
+
+
+class MissingDefault(EnvironmentError):
+    pass
