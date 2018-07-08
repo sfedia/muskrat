f8b192c0cbfd949415ca83db7e510ba4818e6d96
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-09 02:51:33 +0300

    ObjectGrouping & ObjectGroup not more used

--- muskrat/parser.py
+++ muskrat/parser.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from .pattern import *
+
 class Parser:
     def __init__(self):
         self.objects = []
@@ -16,11 +18,21 @@ class Parser:
 
 
 class ParsingObject:
-    def __init__(self, content, object_type):
+    def __init__(self, content, parser, allocator):
         self.connected_objects = []
         self.content = content
-        self.properties = ObjectProperties()
-        self.object_type = object_type
+        self._pattern = None
+        for tracker in Tracker.__subclasses__():
+            tracking = tracker(parser, allocator)
+            if tracking.track():
+                self._pattern = tracking.pattern
+                break
+
+    @property
+    def pattern(self):
+        if self._pattern is None:
+            raise PatternNotFound()
+        return self._pattern
 
     def connect(self, object2connect):
         self.connected_objects.append(object2connect)
@@ -42,52 +54,6 @@ class ParsingObject:
         self.content += content2insert
         if update_function is not None:
             self.content = update_function(self.content)
-
-
-class ObjectGrouping:
-    def __init__(self, object_type=None):
-        self.object_type = object_type
-        self.groups = []
-
-    def set_object_type(self, object_type):
-        if object_type is not None:
-            raise ValueError()
-        self.object_type = object_type
-
-    def add_group(self, group_key, update_function=None):
-        if update_function:
-            group_key = update_function(group_key)
-        self.groups.append(ObjectGroup(group_key))
-
-    def append_to_last_group(self, obj):
-        self.groups[-1].append_object(obj)
-
-    def get_last(self):
-        if self.groups:
-            return self.groups[-1].get_last()
-
-    def get_last_by_type(self, object_type):
-        if object_type == self.object_type:
-            return self.get_last()
-
-    def connect(self, object2connect):
-        self.get_last().connect(object2connect)
-
-    def insert_content(self, content2insert, update_function=None):
-        self.get_last().insert_content(content2insert, update_function)
-
-
-class ObjectGroup:
-    def __init__(self, group_key):
-        self.group_key = group_key
-        self.subobjects = []
-
-    def get_last(self):
-        return self.subobjects[-1]
-
-    def append_object(self, obj2append):
-        self.subobjects.append(obj2append)
-
 
 class LineProcessing:
     def __init__(self, line_text):
