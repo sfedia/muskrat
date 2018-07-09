64d09463ce21bcabedce2e7a79b2011fc953c01d
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-09 23:50:51 +0300

    +import statements & metadata for __init__

--- muskrat/__init__.py
+++ muskrat/__init__.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
-from .parser import *
-from .
+from . import allocator
+from . import connectivity
+from . import defaults
+from . import filters
+from . import parser
+from . import pattern
+from . import xml_generator
+
+
+__author__ = "Fyodor Sizov"
+__copyright__ = "Copyright 2018, Fyodor Sizov"
+__license__ = "GPL"
+__version__ = "3.0"
+__email__ = "prodotiscus@gmail.com"
