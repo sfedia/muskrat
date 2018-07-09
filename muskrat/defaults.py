39914c1b9c12c2f0dfa88458ba6b9730d9e9ac54
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-09 23:38:57 +0300

    VersionOutOfDate + feature_coming

--- muskrat/defaults.py
+++ muskrat/defaults.py
@@ -3,3 +3,9 @@
 import sys
 
 max_connection_depth = sys.getrecursionlimit()
+feature_coming = """This feature is coming soon (check for the next version: see \
+https://github.com/prodotiscus/muskrat or use PyPI)"""
+
+
+class VersionOutOfDate(Exception):
+    pass
