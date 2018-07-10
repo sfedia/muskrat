9a42b2eff92b4bd27bede6040e3b122570a761fa
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-10 05:10:08 +0300

    create defaults.defaults to enable configuration

--- muskrat/defaults.py
+++ muskrat/defaults.py
@@ -2,9 +2,17 @@
 
 import sys
 
-max_connection_depth = sys.getrecursionlimit()
 feature_coming = """This feature is coming soon (check for the next version: see \
 https://github.com/prodotiscus/muskrat or use PyPI)"""
+
+
+class Defaults:
+    def __init__(self):
+        self.max_connection_depth = sys.getrecursionlimit()
+        self.methods_priority = dict(connect=0, insert=1, append=2)
+
+
+defaults = Defaults()
 
 
 class VersionOutOfDate(Exception):
