2565ee3eedd65cfd3e025a1a04236006adffeb0b
Fyodor Sizov <f.sizov@yandex.ru>
2018-07-13 19:23:58 +0300

    add=<int> optional arg

--- muskrat/allocator.py
+++ muskrat/allocator.py
@@ -68,10 +68,10 @@ class Allocator:
         self.make_units()
         self.start_move()
 
-    def next(self):
-        if self.current >= len(self.units) - 1:
+    def next(self, add=0):
+        if self.current + add >= len(self.units) - 1:
             return None
-        return self.units[self.current + 1]
+        return self.units[self.current + 1 + add]
 
     def move_right(self):
         current = self.units[self.current]
