321b942f362aeeaa3ad6bd8b202ce829006b5d7d
root <root@f.sizov.fvds.ru>
2018-07-14 08:42:57 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="muskrat",
-    version="0.0.1",
+    version="0.0.3",
     author="Fyodor Sizov",
     author_email="f.sizov@yandex.ru",
     description="Minimalistic non-BNF text parser",
