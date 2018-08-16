fca064f8c5a4ac8e1a404e239b771f333718e653
Fyodor Sizov <f.sizov@yandex.ru>
2018-08-16 17:00:40 +0300

    2 tests for parser.get(...)

---
+++ tests/test_parser-get.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python3
+
+import muskrat
+from muskrat.parser import *
+from muskrat.allocator import *
+from muskrat.connectivity import Accept, Attach
+
+from . import scan_row, object_model
+
+
+class Transition(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "Transition",
+            Accept().add_default(connect=False, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+
+
+class TransitionTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = Transition()
+        self.extractor = CharSequenceString(">")
+
+    def track(self):
+        return True
+
+
+class AType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "A",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=False, insert=False)
+        )
+
+
+class ATypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = AType()
+        self.extractor = CharSequenceString("A")
+
+    def track(self):
+        return True
+
+
+class BType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "B",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+        self.focus_on = lambda p, c: p.get(2)
+
+
+class BTypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = BType()
+        self.extractor = CharSequenceString("B")
+
+    def track(self):
+        return True
+
+
+class CType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "C",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+        self.focus_on = lambda p, c: p.get(2)
+
+
+class CTypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = CType()
+        self.extractor = CharSequenceString("C")
+
+    def track(self):
+        return True
+
+
+class DType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "D",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+        self.focus_on = lambda p, c: p.get(2)
+
+
+class DTypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = DType()
+        self.extractor = CharSequenceString("D")
+
+    def track(self):
+        return True
+
+
+def test_main():
+    hierarchy_string = "A>B>C>D"
+    parser = Parser()
+    allocator = Allocator(hierarchy_string, muskrat.allocator.WhitespaceVoid(), parser)
+    allocator.start()
+    assert scan_row(
+        parser.objects,
+        [
+            object_model(
+                type="A",
+                content="A",
+                connected=[
+                    object_model(
+                        type="Transition",
+                        content=">",
+                        connected=[]
+                    ),
+                    object_model(
+                        type="B",
+                        content="B",
+                        connected=[
+                            object_model(
+                                type="Transition",
+                                content=">",
+                                connected=[]
+                            ),
+                            object_model(
+                                type="C",
+                                content="C",
+                                connected=[
+                                    object_model(
+                                        type="Transition",
+                                        content=">",
+                                        connected=[]
+                                    ),
+                                    object_model(
+                                        type="D",
+                                        content="D",
+                                        connected=[]
+                                    )
+                                ]
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+
+
+def test_negative():
+    hierarchy_string = "A>B>C>D"
+    parser = Parser()
+    allocator = Allocator(hierarchy_string, muskrat.allocator.WhitespaceVoid(), parser)
+    allocator.start()
+    assert not scan_row(
+        parser.objects,
+        [
+            object_model(
+                type="A",
+                content="A",
+                connected=[
+                    object_model(
+                        type="Transition",
+                        content=">",
+                        connected=[]
+                    ),
+                    object_model(
+                        type="B",
+                        content="B",
+                        connected=[
+                            object_model(
+                                type="Transition",
+                                content=">",
+                                connected=[]
+                            )
+                        ]
+                    ),
+                    object_model(
+                        type="C",
+                        content="C",
+                        connected=[
+                            object_model(
+                                type="Transition",
+                                content=">",
+                                connected=[]
+                            ),
+                            object_model(
+                                type="D",
+                                content="D",
+                                connected=[]
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
