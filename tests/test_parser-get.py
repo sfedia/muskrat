88d357c99a201b734daf15614d4a4e3e957a7b87
Fyodor Sizov <f.sizov@yandex.ru>
2018-08-17 15:11:48 +0300

    reload module when running new test

--- tests/test_parser-get.py
+++ tests/test_parser-get.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+import importlib
 import muskrat
+importlib.reload(muskrat)
 from muskrat.parser import *
 from muskrat.allocator import *
 from muskrat.connectivity import Accept, Attach
@@ -8,104 +10,114 @@ from muskrat.connectivity import Accept, Attach
 from . import scan_row, object_model
 
 
+class Transition(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "Transition",
+            Accept().add_default(connect=False, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+
+
+class TransitionTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = Transition()
+        self.extractor = CharSequenceString(">")
+
+    def track(self):
+        return True
+
+
+class AType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "A",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=False, insert=False)
+        )
+
+
+class ATypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = AType()
+        self.extractor = CharSequenceString("A")
+
+    def track(self):
+        return True
+
+
+class BType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "B",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+        self.focus_on = lambda p, c: p.get(2)
+
+
+class BTypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = BType()
+        self.extractor = CharSequenceString("B")
+
+    def track(self):
+        return True
+
+
+class CType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "C",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+        self.focus_on = lambda p, c: p.get(2)
+
+
+class CTypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = CType()
+        self.extractor = CharSequenceString("C")
+
+    def track(self):
+        return True
+
+
+class DType(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "D",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=True, insert=False)
+        )
+        self.focus_on = lambda p, c: p.get(2)
+
+
+class DTypeTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = DType()
+        self.extractor = CharSequenceString("D")
+
+    def track(self):
+        return True
+
+
 res_main = []
 res_negative = []
 
 
 def test_main():
-    class Transition(Pattern):
-        def __init__(self):
-            Pattern.__init__(
-                self,
-                "Transition",
-                Accept().add_default(connect=False, insert=False),
-                Attach().add_default(connect=True, insert=False)
-            )
-
-    class TransitionTr(Tracker):
-        def __init__(self, *args):
-            Tracker.__init__(self, *args)
-            self.pattern = Transition()
-            self.extractor = CharSequenceString(">")
-
-        def track(self):
-            return True
-
-    class AType(Pattern):
-        def __init__(self):
-            Pattern.__init__(
-                self,
-                "A",
-                Accept().add_default(connect=True, insert=False),
-                Attach().add_default(connect=False, insert=False)
-            )
-
-    class ATypeTr(Tracker):
-        def __init__(self, *args):
-            Tracker.__init__(self, *args)
-            self.pattern = AType()
-            self.extractor = CharSequenceString("A")
-
-        def track(self):
-            return True
-
-    class BType(Pattern):
-        def __init__(self):
-            Pattern.__init__(
-                self,
-                "B",
-                Accept().add_default(connect=True, insert=False),
-                Attach().add_default(connect=True, insert=False)
-            )
-            self.focus_on = lambda p, c: p.get(2)
-
-    class BTypeTr(Tracker):
-        def __init__(self, *args):
-            Tracker.__init__(self, *args)
-            self.pattern = BType()
-            self.extractor = CharSequenceString("B")
-
-        def track(self):
-            return True
-
-    class CType(Pattern):
-        def __init__(self):
-            Pattern.__init__(
-                self,
-                "C",
-                Accept().add_default(connect=True, insert=False),
-                Attach().add_default(connect=True, insert=False)
-            )
-            self.focus_on = lambda p, c: p.get(2)
-
-    class CTypeTr(Tracker):
-        def __init__(self, *args):
-            Tracker.__init__(self, *args)
-            self.pattern = CType()
-            self.extractor = CharSequenceString("C")
-
-        def track(self):
-            return True
-
-    class DType(Pattern):
-        def __init__(self):
-            Pattern.__init__(
-                self,
-                "D",
-                Accept().add_default(connect=True, insert=False),
-                Attach().add_default(connect=True, insert=False)
-            )
-            self.focus_on = lambda p, c: p.get(2)
-
-    class DTypeTr(Tracker):
-        def __init__(self, *args):
-            Tracker.__init__(self, *args)
-            self.pattern = DType()
-            self.extractor = CharSequenceString("D")
-
-        def track(self):
-            return True
-
     hierarchy_string = "A>B>C>D"
     parser = Parser()
     allocator = Allocator(hierarchy_string, muskrat.allocator.WhitespaceVoid(), parser)
@@ -125,3 +137,24 @@ def test_main():
     print("\n".join(res_main))
 
     assert "\n".join(res_main) == sample1
+
+
+def test_negative():
+    hierarchy_string = "A>B>C>D"
+    parser = Parser()
+    allocator = Allocator(hierarchy_string, muskrat.allocator.WhitespaceVoid(), parser)
+    allocator.start()
+
+    tree = muskrat.txt_tree_generator.TXTTree(parser.objects, 2)
+    res_negative = []
+
+    def add_to_buffer(message):
+        global res_negative
+        res_negative.append(message)
+
+    tree.print = add_to_buffer
+    tree.build()
+
+    sample2 = open("./tests/pg1_result2.txt", "r", encoding="utf-8").read()
+
+    assert "\n".join(res_negative) != sample2
