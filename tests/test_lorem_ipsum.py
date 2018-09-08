15a2ebf805c4051e5fb9a7810e72c05bb9ca1c5e
Fyodor Sizov <f.sizov@yandex.ru>
2018-09-08 20:17:03 +0300

    updated test_lorem_ipsum

--- tests/test_lorem_ipsum.py
+++ tests/test_lorem_ipsum.py
@@ -15,107 +15,68 @@ from muskrat.connectivity import Accept, Attach
 from . import scan_row, object_model
 
 
+class Latin(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "Latin",
+            Accept().add_default(connect=True, insert=False),
+            Attach().add_default(connect=False, insert=False)
+        )
+
+
+class LatinTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = Latin()
+        self.extractor = RegexString('[A-Za-z]+')
+
+    def track(self):
+        return True
+
+
+class Digit(Pattern):
+    def __init__(self):
+        Pattern.__init__(
+            self,
+            "Digit",
+            Accept().add_default(connect=False, insert=False),
+            Attach().add_option(muskrat.filters.by_type("Latin"), connect=True, insert=False)
+        )
+
+
+class DigitTr(Tracker):
+    def __init__(self, *args):
+        Tracker.__init__(self, *args)
+        self.pattern = Digit()
+        self.extractor = RegexString(r'\d+')
+
+    def track(self):
+        try:
+            return self.parser.get(1).pattern.object_type == "Latin"
+        except AttributeError:
+            return False
+
+
 def test_main():
-    class Latin(Pattern):
-        def __init__(self):
-            Pattern.__init__(
-                self,
-                "Latin",
-                Accept().add_default(connect=True, insert=False),
-                Attach().add_default(connect=False, insert=False)
-            )
-
-    class LatinTr(Tracker):
-        def __init__(self, *args):
-            Tracker.__init__(self, *args)
-            self.pattern = Latin()
-            self.extractor = RegexString('[A-Za-z]+')
-
-        def track(self):
-            return True
-
-    class Digit(Pattern):
-        def __init__(self):
-            Pattern.__init__(
-                self,
-                "Digit",
-                Accept().add_default(connect=False, insert=False),
-                Attach().add_option(muskrat.filters.by_type("Latin"), connect=True, insert=False)
-            )
-
-    class DigitTr(Tracker):
-        def __init__(self, *args):
-            Tracker.__init__(self, *args)
-            self.pattern = Digit()
-            self.extractor = RegexString(r'\d+')
-
-        def track(self):
-            try:
-                return self.parser.get(1).pattern.object_type == "Latin"
-            except AttributeError:
-                return False
-
     parser = Parser()
     text = "lorem1 ipsum2 dolor3 sit4 amet5"
     allocator = Allocator(text, muskrat.allocator.WhitespaceVoid(), parser)
     allocator.start()
-    assert scan_row(
-        parser.objects,
-        [
-            object_model(
-                type="Latin",
-                content="lorem",
-                connected=[
-                    object_model(
-                        type="Digit",
-                        content="1",
-                        connected=[]
-                    )
-                ]
-            ),
-            object_model(
-                type="Latin",
-                content="ipsum",
-                connected=[
-                    object_model(
-                        type="Digit",
-                        content="2",
-                        connected=[]
-                    )
-                ]
-            ),
-            object_model(
-                type="Latin",
-                content="dolor",
-                connected=[
-                    object_model(
-                        type="Digit",
-                        content="3",
-                        connected=[]
-                    )
-                ]
-            ),
-            object_model(
-                type="Latin",
-                content="sit",
-                connected=[
-                    object_model(
-                        type="Digit",
-                        content="4",
-                        connected=[]
-                    )
-                ]
-            ),
-            object_model(
-                type="Latin",
-                content="amet",
-                connected=[
-                    object_model(
-                        type="Digit",
-                        content="5",
-                        connected=[]
-                    )
-                ]
-            )
-        ]
-    )
+
+    tree = muskrat.txt_tree_generator.TXTTree(parser.objects, 2)
+
+    res_main = []
+
+    def add_to_buffer(message):
+        global res_main
+        res_main.append(message)
+
+    tree.print = add_to_buffer
+    tree.build()
+
+    li1_sample = open("./tests/li1_result1.txt", "r", encoding="utf-8").read()
+
+    print("\n".join(res_main))
+
+    assert "\n".join(res_main) == li1_sample
