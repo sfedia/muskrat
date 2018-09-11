780e65ce9759d50f21854fb2b8da3c1562b9e9ef
Fyodor Sizov <f.sizov@yandex.ru>
2018-09-11 03:37:47 +0300

    new release (1.0.10)

--- muskrat/__init__.py
+++ muskrat/__init__.py
@@ -32,5 +32,5 @@ from . import xml_generator
 __author__ = "Fyodor Sizov"
 __copyright__ = "Copyright 2018, Fyodor Sizov"
 __license__ = "GPL 3.0"
-__version__ = "1.0.9"
+__version__ = "1.0.10"
 __email__ = "prodotiscus@gmail.com"
