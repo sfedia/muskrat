22bd8dabd7c9db86f4247b7ef50d1b369e78145a
Fyodor Sizov <f.sizov@yandex.ru>
2018-09-19 02:58:54 +0300

    +DiveIterator; use it inside .get() and .give() methods

--- muskrat/parser.py
+++ muskrat/parser.py
@@ -37,18 +37,11 @@ class Parser:
         :param condition: object-checking condition if the form of function
         :rtype: ParsingObject
         """
-        if self.depth_limit and behind > self.depth_limit:
-            return None
+        dit = DiveIterator(self.objects, behind, condition, self.depth_limit)
+        for fg_child in dit:
+            pass
 
-        for obj in reversed(self.objects):
-            fc, behind = obj.dive(behind, condition)
-            if fc is not None:
-                return fc
-            if condition(obj):
-                behind -= 1
-            if not behind:
-                return obj
-        return None
+        return dit.this if dit.this else None
 
     def append(self, obj):
         """
@@ -90,25 +83,12 @@ class ParsingObject:
         """
         self.connected_objects.append(object2connect)
 
-    def dive(self, counter=1, condition=lambda obj: True):
-        """
-        Internal method used to find connected (child) objects
-        :param counter: number of element to select which satisfies the condition
-        :type counter: int
-        :param condition: object-checking condition if the form of function
-        :rtype: (ParsingObject, int)
-        """
-        for obj in reversed(self.connected_objects):
-            found_child, counter = obj.dive(counter, condition)
-            if found_child is not None:
-                return found_child, counter
-            if condition(obj):
-                counter -= 1
-            if counter < 0:
-                break
-            elif not counter:
-                return obj, 0
-        return None, counter
+    def dive(self, behind=1, condition=lambda obj: True, depth_limit=None):
+        dit = DiveIterator(self.connected_objects, behind, condition, depth_limit)
+        for fg_child in dit:
+            pass
+
+        return dit.this if dit.this else None
 
     def insert_content(self, content2insert, update_function=None):
         """
@@ -120,3 +100,50 @@ class ParsingObject:
         self.content += content2insert
         if update_function is not None:
             self.content = update_function(self.content)
+
+
+class DiveIterator:
+    def __iter__(self):
+        return self
+
+    def __init__(self, objects, behind=1, condition=lambda obj: True, depth_limit=None):
+        self.objects = list(reversed(objects))
+        self.behind = behind
+        self.condition = condition
+        self.depth_limit = depth_limit
+        self.current_object = 0
+        self.counter = 0
+        self.this = None
+
+    def __next__(self):
+        if self.current_object == len(self.objects):
+            raise StopIteration
+
+        child_di = DiveIterator(
+            self.objects[self.current_object].connected_objects,
+            self.behind,
+            self.condition,
+            self.depth_limit
+        )
+
+        for o in child_di:
+            pass
+
+        if not child_di.behind:
+            return child_di.this
+        else:
+            self.behind = child_di.behind
+
+        self.counter += child_di.counter
+
+        if self.depth_limit is not None and self.depth_limit >= self.counter:
+            raise StopIteration
+
+        if self.condition(self.objects[self.current_object]):
+            if child_di.behind == 1:
+                return self.objects[self.current_object]
+            else:
+                self.behind -= 1
+                self.this = self.objects[self.current_object]
+
+        self.current_object += 1
