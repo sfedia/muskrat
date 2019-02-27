14e513116bd33bd3307b617bddac694b39a0beae
Fyodor Sizov <f.sizov@yandex.ru>
2019-02-28 00:26:42 +0300

    upd version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="muskrat",
-    version="1.0.12",
+    version="1.0.13",
     author="Fyodor Sizov",
     author_email="f.sizov@yandex.ru",
     description="Minimalistic non-BNF text parser",
